36720de44e77f8f3f384b4889de9013339a55c7c
hasib <hasibpanjshery2@gmail.com>
2023-07-16 17:27:53 +0500

    Numbers

--- _02_Numbers.py
+++ _02_Numbers.py
@@ -1,7 +1,4 @@
 n1, n2, n3 = 10, 20.0 ,30
-add, sub, mul, div = (n1 + n2 + n3), (n1 - n2), (n1 * n2), (n1 / n2); 
+add, sub, mul, div, exponential = (n1 + n2 + n3), (n1 - n2), (n1 * n2), (n1 / n2), (2 ** 3) 
 
-print(f" {add} \n {sub} \n {mul} \n {div} ")
-
-exponential = n2 ** n1
-print(exponential)
+print(f" {add} \n {sub} \n {mul} \n {div} \n {exponential}")
