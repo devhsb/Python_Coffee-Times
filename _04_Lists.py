ad1a382d2566888b4a1f5921fc82b1d391c1f9f3
hasib <hasibpanjshery2@gmail.com>
2023-07-19 19:28:32 +0500

    lists update

--- _04_Lists.py
+++ _04_Lists.py
@@ -1,13 +1,6 @@
 
 
-############## Add, Remove , Modify ##############
-# Methods
-    # CREATE 
-        # append(value)  insert(index, value)    
-    # REMOVE
-        # del   pop(index)   remove(value)
-    # UPDATE
-        # list[index] = "value"
+print('\n\n############## Add, Remove , Modify ##############')
 
 list = ["item1, item2, item3"]
 print(list)
@@ -41,7 +34,7 @@ list[2] = 3
 print(list)
 
 
-############## list length ##############
+print('\n\n############## list length ##############')
 # List length
 list = [1,2,3,4,5]
 length = len(list)
@@ -49,7 +42,7 @@ print(length)
 
 
 
-############## Sorting ##############
+print('\n\n############## Sorting ##############')
 # Methods
     # sort()    sort(reverse = True)
     # sorted(list)
@@ -73,3 +66,40 @@ print(list)
 list = [5,4,3,2,1]
 list.reverse()
 print(list)
+
+
+print('\n\n############## Slice ##############')
+list = [1,2,3,4,5,6]
+print(list)
+
+print("#print first 3 values")
+print( list[:3] )
+
+print("#print middle values")
+print( list[2:4] )
+
+print("#print 3 last values")
+print( list[-3:] )
+
+
+print('\n\n############## Copy List ##############')
+list1 = [1,2,3,4]
+list2 = list1
+print(f'list1: {list1}')
+print(f'list2: {list2}')
+
+list1.append(5)
+list2.append(55)
+print(f'list1: {list1}')
+print(f'list2: {list2}') #both print 1,2,3,4,5,55 because with this copy method python associate one list to another
+
+print('\n\n############## Copy List Using Slice ##############')
+list1 = [1,2,3,4]
+list2 = list1[:] #copy items from 0 index to the last
+print(f'list1: {list1}')
+print(f'list2: {list2}')
+
+list1.append(5)
+list2.append(55)
+print(f'list1: {list1}')
+print(f'list2: {list2}') #here each list is individual and not assiciate with each other
