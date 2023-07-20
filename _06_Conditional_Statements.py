7929147b9712ce571b6feacfc992da156b4f4470
hasib <hasibpanjshery2@gmail.com>
2023-07-21 03:12:48 +0500

    Conditional statement

--- _06_Conditional_Statements.py
+++ _06_Conditional_Statements.py
@@ -14,3 +14,14 @@ if( ('Mouyed' in family) and ('Ahmad' not in family) or (len(family) == 5) ):
     print('Yoho, We are here')
 else:
     print('Miss you guys')
+
+
+print('########## if-elif-else ##########')
+if family[0] == 'haseeb':
+    print('iam Haseeb')
+elif family[1].lower() == 'Emran':
+    print('iam Emran')
+elif family[2] == 'Mouyed':
+    print('Iam mouyed')
+else:
+    print('iam not part of this family :(')
