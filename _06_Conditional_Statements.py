fdd8f1b15ecd30695ddd51fba66fce2b6431cbb3
hasib <hasibpanjshery2@gmail.com>
2023-07-21 02:32:29 +0500

    Conditional statement

---
+++ _06_Conditional_Statements.py
@@ -0,0 +1,5 @@
+family = ['Haseeb', 'Emran', 'Mouyed']
+if( ('Mouyed' in family) and ('Ahmad' not in family) or (len(family) == 5) ):
+    print('Yoho, We are here')
+else:
+    print('Miss you guys')
